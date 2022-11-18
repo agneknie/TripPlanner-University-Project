b7fa455eaf27bb7e6f12fff5befa552c9217f9d7
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2022-11-18 20:12:04 +0000

    Entities with fields

--- app/src/main/java/com/example/tripplanner/models/Trip.kt
+++ app/src/main/java/com/example/tripplanner/models/Trip.kt
@@ -1,4 +1,24 @@
 package com.example.tripplanner.models
 
-class Trip {
+data class Trip(
+    val tripId: Int = 0,
+    val startDateTime: String,
+    var endDateTime: String? = null,
+    val title: String,
+    var tagId: Int? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Trip
+
+        if (tripId != other.tripId) return false
+        if (startDateTime != other.startDateTime) return false
+        if (endDateTime != other.endDateTime) return false
+        if (title != other.title) return false
+        if (tagId != other.tagId) return false
+
+        return true
+    }
 }
