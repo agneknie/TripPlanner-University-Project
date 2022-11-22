eb9abfae98764b0753df59f74882c532410ee8ff
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2022-11-22 00:56:07 +0000

    View of TripTripActivity

--- app/src/main/java/com/example/tripplanner/views/TripTripActivity.kt
+++ app/src/main/java/com/example/tripplanner/views/TripTripActivity.kt
@@ -11,5 +11,7 @@ class TripTripActivity: TripPlannerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTripTripBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // TODO Insert map into activity_trip_trip_ll_map_holder. Set inserted map's height & width to match parent.
     }
 }
