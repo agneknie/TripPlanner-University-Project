5bd3670b5197b07c9f70db60a1d1d5579d340d6c
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2022-11-26 21:58:55 +0000

    Overriding back button functionality on TripTripActivity

--- app/src/main/java/com/example/tripplanner/views/TripTripActivity.kt
+++ app/src/main/java/com/example/tripplanner/views/TripTripActivity.kt
@@ -1,8 +1,10 @@
 package com.example.tripplanner.views
 
 import android.os.Bundle
+import com.example.tripplanner.R
 import com.example.tripplanner.TripPlannerAppCompatActivity
 import com.example.tripplanner.databinding.ActivityTripTripBinding
+import com.google.android.material.snackbar.Snackbar
 
 class TripTripActivity: TripPlannerAppCompatActivity() {
     private lateinit var binding: ActivityTripTripBinding
@@ -13,5 +15,10 @@ class TripTripActivity: TripPlannerAppCompatActivity() {
         setContentView(binding.root)
 
         // TODO Insert map into activity_trip_trip_ll_map_holder. Set inserted map's height & width to match parent.
+    }
+
+    @Deprecated("Declaration overrides deprecated member but not marked as deprecated itself")
+    override fun onBackPressed() {
+        this.displaySnackbar(binding.root, R.string.finish_trip_before_exiting_snackbar, Snackbar.LENGTH_LONG)
     }
 }
