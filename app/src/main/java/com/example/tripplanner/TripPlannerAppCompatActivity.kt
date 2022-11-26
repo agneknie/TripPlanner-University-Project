b89e45ada3807e7c6e6f499980014ed107236787
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2022-11-26 21:49:20 +0000

    Convenience method for displaying snackbar

--- app/src/main/java/com/example/tripplanner/TripPlannerAppCompatActivity.kt
+++ app/src/main/java/com/example/tripplanner/TripPlannerAppCompatActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tripplanner
 
+import android.view.View
 import com.example.tripplanner.viewmodels.TripPlannerViewModel
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tripplanner.viewmodels.TripPlannerViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * Instantiates ViewModels of the application.
@@ -18,5 +20,17 @@ open class TripPlannerAppCompatActivity: AppCompatActivity() {
             (application as TripPlannerApplication).photoRepository,
             (application as TripPlannerApplication).tagRepository,
             application)
+    }
+
+    /**
+     * Convenience method for displaying a Snackbar.
+     */
+    protected fun displaySnackbar(view: View, messageStringId: Int, displayLength: Int){
+        val snackbar = Snackbar.make(view, messageStringId, displayLength)
+
+        snackbar.setBackgroundTint(this.getColor(R.color.main_colour))
+        snackbar.setTextColor(this.getColor(R.color.app_background))
+
+        snackbar.show()
     }
 }
