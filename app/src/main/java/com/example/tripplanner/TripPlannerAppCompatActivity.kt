221a6262e26ec3caef0b79c7632ca682f21cfc41
Agne Knietaite <aknietaite1@sheffield.ac.uk>
2022-11-28 00:43:43 +0000

    Amending displaySnackbar to use resource ids and strings

--- app/src/main/java/com/example/tripplanner/TripPlannerAppCompatActivity.kt
+++ app/src/main/java/com/example/tripplanner/TripPlannerAppCompatActivity.kt
@@ -24,10 +24,26 @@ open class TripPlannerAppCompatActivity: AppCompatActivity() {
 
     /**
      * Convenience method for displaying a Snackbar.
+     * Uses resource id to locate a string.
      */
-    fun displaySnackbar(view: View, messageStringId: Int, displayLength: Int){
-        val snackbar = Snackbar.make(view, messageStringId, displayLength)
+    fun displaySnackbar(view: View, messageStringId: Int){
+        val snackbar = Snackbar.make(view, messageStringId, Snackbar.LENGTH_LONG)
+        configureAndShowSnackbar(snackbar)
+    }
 
+    /**
+     * Convenience method for displaying a Snackbar.
+     * Uses string and just displays it.
+     */
+    fun displaySnackbar(view: View, messageString: String){
+        val snackbar = Snackbar.make(view, messageString, Snackbar.LENGTH_LONG)
+        configureAndShowSnackbar(snackbar)
+    }
+
+    /**
+     * Sets snackbar's colour and shows it.
+     */
+    private fun configureAndShowSnackbar(snackbar: Snackbar){
         snackbar.setBackgroundTint(this.getColor(R.color.main_colour))
         snackbar.setTextColor(this.getColor(R.color.app_background))
 
